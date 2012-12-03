62a196e6845a04d8b405c6ec90a08dcb861e4cab
green <green@10.0.0.2>
2012-12-03 04:46:28 +0100

    start button

---
+++ ue3/SysArch-ue3/src/bildverarbeitung/filter/Start.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bildverarbeitung.filter;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author green
+ */
+public class Start extends JButton implements ActionListener{
+ 
+    
+    public Start(){
+        this.setText("Start");
+    }
+
+    
+    public void addActionListner(ActionListener l){
+        
+    }
+    public void removeActionListener(ActionListener l){
+        
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+    
+    
+    
+}
