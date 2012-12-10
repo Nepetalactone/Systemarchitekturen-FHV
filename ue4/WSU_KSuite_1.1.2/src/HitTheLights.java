2fea90110c51a2f6e804a3424657f2601dc636a7
green <green@sme3863-notebook.air.uclv.net>
2012-12-10 14:40:53 +0100

    asdf

--- ue4/WSU_KSuite_1.1.2/src/HitTheLights.java
+++ ue4/WSU_KSuite_1.1.2/src/HitTheLights.java
@@ -18,25 +18,16 @@ public class HitTheLights extends RobotController {
     @Override
     public void doWork() throws Exception {
 
-        if (this.getLightValue(2) < 400 || this.getLightValue(3) < 400) {
-            if(this.getLightValue(2) < this.getLightValue(3)){
-                this.setMotorSpeeds(2, 5);
-            }else{
-                this.setMotorSpeeds(5, 2);
-            }
-        }else if (getDistanceValue(2) > 50) {
-            setMotorSpeeds(2, -5);
-        } else if (getDistanceValue(3) > 50) {
-            setMotorSpeeds(-5, 2);
-        } else if (getDistanceValue(1) > 50) {
-            setMotorSpeeds(5, -5);
-        } else if (getDistanceValue(4) > 50) {
-            setMotorSpeeds(-5, 5);
-        } else if (getDistanceValue(0) > 50) {
-            setMotorSpeeds(5, -5);
-        } else if (getDistanceValue(5) > 50) {
-            setMotorSpeeds(-5, 5);
-        } else {
+        
+        if(this.getLightValue(2) < 400 || this.getLightValue(3) < 400){
+            this.setMotorSpeeds(5, 5);
+        }else if(this.getLightValue(0) < 400 || this.getLightValue(1) < 400){
+            this.setMotorSpeeds(-5, 5);
+        }else if(this.getLightValue(4) < 400 || this.getLightValue(5) < 400){
+            this.setMotorSpeeds(5, -5);
+        }else if(this.getLightValue(6) < 400 || this.getLightValue(7) < 400){
+            this.setMotorSpeeds(-5, -5);
+        }else{
             this.setMotorSpeeds(5, 5);
         }
     }
