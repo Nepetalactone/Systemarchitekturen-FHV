d4bd869e6d06de04892123dce223411db86b3c2c
green <green@10.0.0.2>
2012-12-12 00:30:34 +0100

    wand nochfahra poi

--- ue4/WSU_KSuite_1.1.2/src/FollowThatWall.java
+++ ue4/WSU_KSuite_1.1.2/src/FollowThatWall.java
@@ -17,13 +17,16 @@ public class FollowThatWall extends RobotController{
         this.setWaitTime(5L);
         
         matrix = new double[][]{
-            {-10, -5, 5, 7,  5,  10, 0, 0},
-            { 10,  5, 7, 5, -5, -10, 0, 0}
+            {-5, -5, 15, 25,  20,  20, 0, 0},
+            { 5,  5, 25, 0, -20, -20, 0, 0}
+            
+                //goht wenn ma koa hindernisse ibaut
 //            {-10, -5, 5, 7,  5,  10, 0, 0},
 //            { 10,  5, 7, 5, -5, -10, 0, 0}
-//            {-5, -5, 5, 5,  5,  5, 0, 0},
-//            { 5,  5, 5, 5, -5, -5, 0, 0}
-            
+                
+                //der fahrt perfekte kurfen um alles, aber dreht sich zersch 1mol im kreis..
+//            {-5, -5, 15, 25,  20,  20, 0, 0},
+//            { 5,  5, 25, 0, -20, -20, 0, 0}
             
         };
                            
@@ -53,11 +56,9 @@ public class FollowThatWall extends RobotController{
         for (int i = 0; i < 8; i++) {
             
             sensors[i] = (1200.0 - getDistanceValue(i)) / 1000.0;
-            
             l += sensors[i] * matrix[0][i];
             r += sensors[i] * matrix[1][i];
         }
-        
         return new int[]{(int) Math.round(l), (int) Math.round(r)};
     }
     
